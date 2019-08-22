e8cc7e8ec970189771e3b88fbba09851e6a5fe3e
Serigne Saliou Gueye <gueyesaliou067@gmail.com>
2019-08-22 19:29:35 +0200

    page _presentation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AcceuilComponent } from './acceuil/acceuil.component';
 import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 const routes: Routes = [
   { path: '', component: AcceuilComponent },
   { path: 'menu', component: MenuComponent },
+  { path: 'footer', component: FooterComponent },
 ];
 
 @NgModule({
